15e9eee360528753692e0efcb44b1fc1e8ac3860
Steve Hu <stevehu@gmail.com>
2018-11-06 19:10:24 -0500

    fixes #74 add taiji and lightapi build

---
+++ light-router/light-portal/lightapi/build/precache-manifest.ee9a9cc4ddf0f6772d247076240ba145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "92ecdffb241bff2dda5d",
+    "url": "/static/js/main.92ecdffb.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "92ecdffb241bff2dda5d",
+    "url": "/static/css/main.d645def8.chunk.css"
+  },
+  {
+    "revision": "2ad83b90c9d2900b2ff1351f52f2770e",
+    "url": "/index.html"
+  }
+];
