63b76a6d0b3831ff6b259c13b1992b140814e957
Steve Hu <stevehu@gmail.com>
2018-11-06 19:00:31 -0500

    fixes #74 update .gitignore in router portal faucet to check in build folder

---
+++ light-router/light-portal/faucet/build/precache-manifest.1d07dd79ef85dbcd96e6fbe90e3f2ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "04acaf2fb2b7f0a049be",
+    "url": "/static/js/main.04acaf2f.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "04acaf2fb2b7f0a049be",
+    "url": "/static/css/main.99f25f04.chunk.css"
+  },
+  {
+    "revision": "881f0073153660a40f082dee167ffc26",
+    "url": "/index.html"
+  }
+];
