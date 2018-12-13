8a504df9167cdef7a6ec02f0fdb31428d176d487
Steve Hu <stevehu@gmail.com>
2018-12-13 16:42:34 -0500

    fixes #92 update web-client home page to provide more links

---
+++ light-router/light-portal/webclient/build/precache-manifest.d6baa2d2f47633c0fa8cd36e505dccf7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "97edb151f0f246823d0d",
+    "url": "/static/js/main.97edb151.chunk.js"
+  },
+  {
+    "revision": "aa6a3907aeb7f8c2d680",
+    "url": "/static/js/1.aa6a3907.chunk.js"
+  },
+  {
+    "revision": "c489360c82eb28f0ff3a2ccea354c671",
+    "url": "/index.html"
+  }
+];
--- light-router/light-portal/webclient/build/service-worker.js
+++ light-router/light-portal/webclient/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95c8baa336b6a020300f945f3544a256.js"
+  "/precache-manifest.d6baa2d2f47633c0fa8cd36e505dccf7.js"
 );
 
 workbox.clientsClaim();
