986015dc3e0896d1ad1a72e33769fcc1e8e1c5a5
Steve Hu <stevehu@gmail.com>
2019-06-29 22:53:01 -0400

    fixes #199 add light-codegen folder for the multiple instances of light-codegen

---
+++ light-codegen/codegen-web/build/precache-manifest.8cc6f890e4c89614587b7bfb1a58c907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "193ac0376d9a18761cc0",
+    "url": "/static/js/main.e459df9c.chunk.js"
+  },
+  {
+    "revision": "ccfab3be02397ac26155",
+    "url": "/static/js/2.876d61e9.chunk.js"
+  },
+  {
+    "revision": "193ac0376d9a18761cc0",
+    "url": "/static/css/main.43395162.chunk.css"
+  },
+  {
+    "revision": "50b1a805403e38c45433898c0d4df974",
+    "url": "/index.html"
+  }
+];
