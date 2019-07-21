090a29e0e5012d4e349ff966ee9ecfaef3de5333
Steve Hu <stevehu@gmail.com>
2019-07-20 22:27:31 -0400

    fixes #229 update light-router local-portal config for the form authentication

---
+++ light-router/local-portal/signin/build/precache-manifest.f41c2c1bf0dc1df2721110f00c29907f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f00e2f5370bc6e48a7ce41b85907bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c67133bd807e1f5a9810",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cbe2359722a3b12b4bf4",
+    "url": "/static/js/2.3406d5f2.chunk.js"
+  },
+  {
+    "revision": "c67133bd807e1f5a9810",
+    "url": "/static/js/main.19dbeb4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
