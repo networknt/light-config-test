44ff7ad7824cbbef568a16d06ebe6a0d6f781657
Steve Hu <stevehu@gmail.com>
2020-01-19 10:51:34 -0500

    fixes #322 move the source code to taiji.io from light-config-test

---
+++ light-router/virtual-host/taiji/build/precache-manifest.d2aafbc57e85d2de2d57671c82a3a841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f008a323da0970f63080c67b85c63c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8915d575cec0780cce6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c346245a4211c82f0223",
+    "url": "/static/js/2.b223b97b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b223b97b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8915d575cec0780cce6",
+    "url": "/static/js/main.0675ea54.chunk.js"
+  },
+  {
+    "revision": "cb620195d9610a42d055",
+    "url": "/static/js/runtime-main.a9fbdc04.js"
+  }
+]);
