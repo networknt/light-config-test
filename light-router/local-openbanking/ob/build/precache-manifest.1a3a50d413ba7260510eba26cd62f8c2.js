b554670db59f2a39695046074135520f03e65552
Steve Hu <stevehu@gmail.com>
2020-02-09 11:40:33 -0500

    fixes #341 update local-openbanking/ob build from react-client

---
+++ light-router/local-openbanking/ob/build/precache-manifest.1a3a50d413ba7260510eba26cd62f8c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7574fa6026d7d8ee52f359b40869282a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d9bddc283729066e26e",
+    "url": "/static/css/main.fd2fdf4a.chunk.css"
+  },
+  {
+    "revision": "ab7efb4cb235f39dfc24",
+    "url": "/static/js/2.c8f7d179.chunk.js"
+  },
+  {
+    "revision": "6b33007e3215da15e3b7998700d4d60a",
+    "url": "/static/js/2.c8f7d179.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d9bddc283729066e26e",
+    "url": "/static/js/main.b9d929d8.chunk.js"
+  },
+  {
+    "revision": "09650f2d1df9f3da4c07",
+    "url": "/static/js/runtime-main.3ae8d8da.js"
+  }
+]);
--- light-router/local-openbanking/ob/build/service-worker.js
+++ light-router/local-openbanking/ob/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95ce655b4921257cc99ea6596a03f1bf.js"
+  "/precache-manifest.1a3a50d413ba7260510eba26cd62f8c2.js"
 );
 
 self.addEventListener('message', (event) => {
