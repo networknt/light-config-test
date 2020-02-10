0e843744f04da81a8c7c5c04dd86f73fa7a9494e
Steve Hu <stevehu@gmail.com>
2020-02-09 20:36:07 -0500

    fixes #343 add light-router test-openbanking for test cloud deployment

---
+++ light-router/test-openbanking/signin/build/precache-manifest.95ce655b4921257cc99ea6596a03f1bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdf31e998d7301a76c22c82bb2089f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb64e45a2febb39e5ffe",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5ffeaf1b86ff55b23ae2",
+    "url": "/static/js/2.428db44b.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.428db44b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb64e45a2febb39e5ffe",
+    "url": "/static/js/main.4e55a631.chunk.js"
+  },
+  {
+    "revision": "3179e5ad5e0ca8b5f171",
+    "url": "/static/js/runtime-main.4abd440d.js"
+  }
+]);
