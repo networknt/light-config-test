9d4c1b769406f6fe5619c18f68ccc2f706c9f718
Steve Hu <stevehu@gmail.com>
2020-02-09 22:56:15 -0500

    fixes #345 update test-portal with the latest signin site from light-oauth2

--- light-router/test-portal/signin/build/service-worker.js
+++ light-router/test-portal/signin/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.beef464f92d3881cc79afdde025460a5.js"
+  "/precache-manifest.95ce655b4921257cc99ea6596a03f1bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
