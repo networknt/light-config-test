60b6e7ec9198484394e86c58a903b01cd3373e2c
Steve Hu <stevehu@gmail.com>
2020-03-01 00:21:16 -0500

    fixes #374 add SaaS compose and login folder

---
+++ light-router/local-openbanking/login/build/precache-manifest.f04ab94f3b93edbd355b0719f04a6ca5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f118306447721a98bb9a10237c619e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0401d09953b22478cff",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a38601b074a12e01eb65",
+    "url": "/static/js/2.0e0100c8.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.0e0100c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0401d09953b22478cff",
+    "url": "/static/js/main.6938d53a.chunk.js"
+  },
+  {
+    "revision": "3179e5ad5e0ca8b5f171",
+    "url": "/static/js/runtime-main.4abd440d.js"
+  }
+]);
