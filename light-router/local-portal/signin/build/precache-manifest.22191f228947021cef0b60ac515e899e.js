43be60079a5a0390abdcdced1fe6e8cb6a5cc2cc
Steve Hu <stevehu@gmail.com>
2020-03-16 21:49:13 -0400

    fixes #380 update light-router local-portal config and signin build

---
+++ light-router/local-portal/signin/build/precache-manifest.22191f228947021cef0b60ac515e899e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c441be2abcc8b9f5038637d0bfff0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab70be170e0b9112b109",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a38601b074a12e01eb65",
+    "url": "/static/js/2.0e0100c8.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.0e0100c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab70be170e0b9112b109",
+    "url": "/static/js/main.986c8ef6.chunk.js"
+  },
+  {
+    "revision": "3179e5ad5e0ca8b5f171",
+    "url": "/static/js/runtime-main.4abd440d.js"
+  }
+]);
--- light-router/local-portal/signin/build/service-worker.js
+++ light-router/local-portal/signin/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.beef464f92d3881cc79afdde025460a5.js"
+  "/precache-manifest.22191f228947021cef0b60ac515e899e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
