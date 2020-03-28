ffdd947134170845bc5b3f1241098575154a23e7
Steve Hu <stevehu@gmail.com>
2020-03-27 20:23:37 -0400

    fixes #389 update light-router local-portal signin build from login-view

---
+++ light-router/local-portal/signin/build/precache-manifest.c1f481abc13da458790246fcafce99db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b462de7528584c5d4099730b6ac3894",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3039cf104e2f7a6609b0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "519da45af16a76d802bf",
+    "url": "/static/js/2.7099edcb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7099edcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3039cf104e2f7a6609b0",
+    "url": "/static/js/main.c8dce6b0.chunk.js"
+  },
+  {
+    "revision": "5aa3ed0e14abaf688599",
+    "url": "/static/js/runtime-main.678d3a0f.js"
+  }
+]);
--- light-router/local-portal/signin/build/service-worker.js
+++ light-router/local-portal/signin/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.22191f228947021cef0b60ac515e899e.js"
+  "/precache-manifest.c1f481abc13da458790246fcafce99db.js"
 );
 
 self.addEventListener('message', (event) => {
