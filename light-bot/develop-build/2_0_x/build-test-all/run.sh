268d399ed49e5b4c983b423576e78819d27870d0
Steve Hu <stevehu@gmail.com>
2026-02-17 17:10:39 -0500

    fixes #874 update build-test-all petstore test and use the maven jar

--- light-bot/develop-build/2_0_x/build-test-all/run.sh
+++ light-bot/develop-build/2_0_x/build-test-all/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dlight-4j-config-dir=./config -Dlogback.configurationFile=./logback.xml -jar ~/networknt/light-bot/bot-cli/build/libs/bot-cli-fat-1.0.jar -t develop-build
+java -Dlight-4j-config-dir=./config -Dlogback.configurationFile=./logback.xml -jar ~/networknt/light-bot/bot-cli/target/bot-cli.jar -t develop-build
